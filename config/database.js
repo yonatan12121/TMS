f04938dceff3e4864cf734b5a833508bb70d3968
yonatan12121 <yonatanmekonnen59@gmail.com>
2024-05-23 12:21:28 +0300

    database config update

--- config/database.js
+++ config/database.js
@@ -4,9 +4,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connectDB = async () => {
+    const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/TMS';
+    
     try {
-        await mongoose.connect(process.env.MONGO_URI, {});
-        console.log('MongoDB connected');
+        await mongoose.connect(mongoURI);
+        console.log(`MongoDB connected successfull`);
     } catch (err) {
         console.error(err.message);
         process.exit(1);
